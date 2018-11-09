cfa2373e3efe8e86a8cc07912faab39bf405a0a0
analuizamtg <analuizamtg@gmail.com>
2018-11-08 22:24:20 -0300

    Update layout of Appointment schedulling screen

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -3,20 +3,75 @@ import "./App.css";
 import styled from "styled-components";
 import Input from "./components/Input";
 import Dropdown from "./components/Dropdown";
+import DatePicker from "react-datepicker";
+import moment from "moment";
+import "react-datepicker/dist/react-datepicker.css";
+import Button from "./components/Button";
+import { compose, graphql } from "react-apollo";
+import createAppointment from "./graphql/CreateAppointment.js";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: moment()
+    };
+  }
+
+  handleClick = () => {
+    const { name, email, date } = this.state;
+    console.log("aquiiii");
+    this.props
+      .createAppointment({
+        variables: {
+          dateAndTime: new Date(),
+          user: { name: name, email: email }
+        }
+      })
+      .then(appointment => {
+        console.log("aquiiiiiii");
+      });
+  };
+
   render() {
     return (
-      <Container>
-        <Div>
-          <Input label={"Full name"} />
-          <Input label={"E-mail"} type={"email"} />
-          <Dropdown
-            options={[{ label: "19:30", value: new Date() }]}
-            label={"Time"}
-          />
-        </Div>
-      </Container>
+      <div>
+        <Container>
+          <h2>Schedule your appointment</h2>
+          <Div>
+            <Input
+              label={"Full name"}
+              onChange={e => {
+                this.setState({ name: e.target.value });
+              }}
+            />
+            <Input
+              label={"E-mail"}
+              onChange={e => {
+                this.setState({ email: e.target.value });
+              }}
+              type={"email"}
+            />
+            <DateTimeContainer>
+              <DatePickerContainer>
+                <DatePicker
+                  customInput={<Input label={"Date"} />}
+                  selected={this.state.date}
+                  onChange={this.handleDateChange}
+                />
+              </DatePickerContainer>
+              <DropdownContainer>
+                <Dropdown
+                  onChange={() => {}}
+                  options={[{ label: "19:30", value: new Date() }]}
+                  label={"Time"}
+                />
+              </DropdownContainer>
+            </DateTimeContainer>
+            <Button onClick={this.handleClick} label={"schedule"} />
+          </Div>
+        </Container>
+      </div>
     );
   }
 }
@@ -28,6 +83,20 @@ const Container = styled.div`
   border-color: #e3e4e6;
   border-radius: 0.25rem;
   margin: auto;
+  padding-top: 1rem;
+`;
+
+const DateTimeContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const DatePickerContainer = styled.div`
+  padding-right: 2rem;
+`;
+
+const DropdownContainer = styled.div`
+  width: 100%;
 `;
 
 const Div = styled.div`
@@ -37,4 +106,8 @@ const Div = styled.div`
   padding-bottom: 1.5rem;
 `;
 
-export default App;
+const createCollectionMutation = graphql(createAppointment, {
+  name: "createAppointment"
+});
+
+export default compose(createCollectionMutation)(App);
