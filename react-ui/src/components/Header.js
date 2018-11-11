24e3aa384276d200f8d6fd59661c312ad7401298
analuizamtg <analuizamtg@gmail.com>
2018-11-11 18:37:01 -0300

    Add page Header component

---
+++ react-ui/src/components/Header.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+const Header = ({ title, width }) => {
+  return (
+    <OuterContainer>
+      <Container width={width}>
+        <InnerContainer>{title}</InnerContainer>
+      </Container>
+    </OuterContainer>
+  );
+};
+
+const Container = styled.div`
+  width: ${props => props.width || "80%"};
+  background-color: #f2f4f5;
+  padding: 2rem;
+  margin: auto;
+`;
+
+const OuterContainer = styled.div`
+  padding-top: 2rem;
+`;
+
+const InnerContainer = styled.div`
+  font-weight: 600;
+  font-size: 2.25rem;
+`;
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+  width: PropTypes.string
+};
+
+export default Header;
